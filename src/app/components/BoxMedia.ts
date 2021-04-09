0c1263811e147571fcbbddf45bce2b0252d7c951
RJ Cabagnot <rjcabagnot@hotmail.com>
2021-04-09 17:27:23 +0200

    updated

---
+++ src/app/components/BoxMedia.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const BoxMedia = styled("div")`
+  max-width: 20rem;
+  margin: 2rem 1rem 2rem 1rem;
+  background-color: lightgray;
+  box-shadow: 2px 3px gray;
+  border-radius: 8px;
+`;
--- src/app/views/UserPage.tsx
+++ src/app/views/UserPage.tsx
@@ -4,6 +4,7 @@ import { getUserAxios } from "../../services/userService";
 import { Button, Container } from "../components/Button";
 import { MediaCard } from "../components/MediaCard";
 import { H1, H2 } from "../components/Typography";
+import { BoxMedia } from "../components/BoxMedia";
 
 const UserPage = () => {
   const [userResponse, setUserResponse] = useState<userResponseModel>();
@@ -21,52 +22,56 @@ const UserPage = () => {
   return (
     <MediaCard>
       {userResponse?.results.map((result, index) => (
-        <div style={{ margin: "3rem" }} key={result.email}>
-          <div>
-            <img src={result.picture.large} alt={"picture"} />
-          </div>
-          <div>
-            <H1 color="blue">
-              Name:{" "}
-              {`${result.name.title} ${result.name.first} ${result.name.last}`}
-            </H1>
-          </div>
-
-          <div>
-            <H2>Age: {result.dob.age}</H2>
-          </div>
-
-          <Container>
-            <div style={{ margin: "1rem" }}>
-              <Button
-                onClick={() => {
-                  const index = userResponse?.results.indexOf(result);
-                  userResponse?.results.splice(index, 1);
-                  setUserResponse({ ...userResponse });
-                }}
-                primary
-              >
-                Hi!
-              </Button>
+        <BoxMedia>
+          <div style={{ margin: "3rem" }} key={result.email}>
+            <div>
+              <img src={result.picture.large} alt={"picture"} />
+            </div>
+            <div>
+              <H1 color="blue">
+                Name:{" "}
+                {`${result.name.title} ${result.name.first} ${result.name.last}`}
+              </H1>
             </div>
 
-            <div style={{ margin: "1rem" }}>
-              <Button
-                onClick={() => {
-                  userResponse.results = userResponse?.results?.filter((r) => {
-                    console.log("R::", r);
-                    return r.id !== result.id;
-                  });
-
-                  setUserResponse({ ...userResponse });
-                }}
-                secondary={false}
-              >
-                Bye!
-              </Button>
+            <div>
+              <H2>Age: {result.dob.age}</H2>
             </div>
-          </Container>
-        </div>
+
+            <Container>
+              <div style={{ margin: "1rem" }}>
+                <Button
+                  onClick={() => {
+                    const index = userResponse?.results.indexOf(result);
+                    userResponse?.results.splice(index, 1);
+                    setUserResponse({ ...userResponse });
+                  }}
+                  primary
+                >
+                  Hi!
+                </Button>
+              </div>
+
+              <div style={{ margin: "1rem" }}>
+                <Button
+                  onClick={() => {
+                    userResponse.results = userResponse?.results?.filter(
+                      (r) => {
+                        console.log("R::", r);
+                        return r.id !== result.id;
+                      }
+                    );
+
+                    setUserResponse({ ...userResponse });
+                  }}
+                  secondary={false}
+                >
+                  Bye!
+                </Button>
+              </div>
+            </Container>
+          </div>
+        </BoxMedia>
       ))}
     </MediaCard>
   );
